45e35838562ac097c7f7d1c7975af9f9667db0c1
Ismael Henrique Da Silva <ismaelhrsilva@gmail.com>
2024-08-24 20:04:15 -0300

    begin ex02

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -1,13 +1,13 @@
-#include "Account.h"
+#include "Account.hpp"
 
 int Account::_nbAccounts = 0;
 int Account::_totalAmount= 0;
 int Account::_totalNbDeposits= 0;
 int Account::_totalNbWithdrawals= 0;
 
-Account::Account( int initial_deposit ):_accountIndex(_accountIndex),
+Account::Account( int initial_deposit ):_accountIndex(_nbAccounts),
 	_amount(initial_deposit) ,_nbDeposits(0), _nbWithdrawals(0) {
-	_accountIndex++;
+	_nbAccounts++;
 	_totalAmount += initial_deposit;
 };
 
@@ -17,3 +17,25 @@ int	Account::getNbAccounts( void ) { return (_nbAccounts);};
 int	Account::getTotalAmount( void ) { return (_totalAmount);};
 int	Account::getNbDeposits( void ) { return (_totalNbDeposits);};
 int	Account::getNbWithdrawals( void ) { return (_totalNbWithdrawals);};
+
+void Account::displayAccountsInfos( void ){};
+void Account::_displayTimestamp( void ){};
+
+void Account::makeDeposit( int deposit ){
+	this->_amount += deposit;
+};
+
+bool	Account::makeWithdrawal( int withdrawal ){
+	this->_amount -= withdrawal;
+	return(1);
+};
+
+int		Account::checkAmount( void ) const{
+	if (_amount >= 0)
+		return(0);
+	return (1);
+};
+
+void	Account::displayStatus( void ) const{
+	return ;
+};
