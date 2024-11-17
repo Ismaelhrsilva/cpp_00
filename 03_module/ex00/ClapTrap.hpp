9cce1fb477c790467e4462aab8b38692e350c21b
Ismael Henrique Da Silva <ismaelhrsilva@gmail.com>
2024-11-17 19:34:35 -0300

    ex00 wip

--- 03_module/ex00/ClapTrap.hpp
+++ 03_module/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: ishenriq <ishenriq@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 19:38:04 by ishenriq          #+#    #+#             */
-/*   Updated: 2024/10/10 20:14:52 by ishenriq         ###   ########.fr       */
+/*   Updated: 2024/11/17 19:14:10 by ishenriq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ class	ClapTrap {
 	public:
 		ClapTrap(void);
 		ClapTrap(std::string name);
-		~ClapTrap(void);
 		ClapTrap(const ClapTrap &other);
 		ClapTrap &operator=(const ClapTrap &other);
+		~ClapTrap(void);
 		
 		void	attack(const std::string &target);
 		void	takeDamage(unsigned int amount);
@@ -35,15 +35,15 @@ class	ClapTrap {
 		void	setAttackDamage(unsigned int const &attackDamage);
 
 		std::string	getName(void) const;
-		unsigned	getHitPoint(void) const;
-		unsigned	getEnergyPoint(void) const;
-		unsigned	getAttackDamage(void) const;
+		unsigned int	getHitPoint(void) const;
+		unsigned int 	getEnergyPoint(void) const;
+		unsigned int	getAttackDamage(void) const;
 
 	private:
-		std::string _name
-		unsigned int	_hitPoint;
-		unsigned int	_energyPoint;
+		std::string 	_name;
+		unsigned int	_hitPoints;
+		unsigned int	_energyPoints;
 		unsigned int	_attackDamage;
-}
+};
 
 #endif
--- 03_module/ex00/main.cpp
+++ 03_module/ex00/main.cpp
@@ -6,16 +6,47 @@
 /*   By: ishenriq <ishenriq@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 20:15:37 by ishenriq          #+#    #+#             */
-/*   Updated: 2024/11/07 20:16:38 by ishenriq         ###   ########.fr       */
+/*   Updated: 2024/11/17 19:33:46 by ishenriq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
-int	main(void){
+int main(void){
 
+	ClapTrap ismael;
+	//ClapTrap samuel("samuel");
+	//ClapTrap a(ismael);
+	//ClapTrap b = samuel;
+	
+	std::cout << std::string(20, '-') << std::endl;
+	std::cout << "Setando os nomes" << std::endl;
+	std::cout << std::string(20, '-') << std::endl;
 
+	std::cout << ismael.getName() << std::endl;
+	ismael.setName("ismael");
+	std::cout << ismael.getName() << std::endl;
 
+	std::cout << ismael.getHitPoint() << std::endl;
+	ismael.setHitPoint(150);
+	std::cout << ismael.getHitPoint() << std::endl;
+
+	std::cout << ismael.getAttackDamage() << std::endl;
+	ismael.setAttackDamage(40);
+	std::cout << ismael.getAttackDamage() << std::endl;
+
+	std::cout << ismael.getEnergyPoint() << std::endl;
+	ismael.setEnergyPoint(100);
+	std::cout << ismael.getEnergyPoint() << std::endl;
+
+	/*ismael.attack("samuel");
+	samuel.takeDamage(ismael.getAttackDamage());
+	samuel.beRepaired(100);*/
+
+	/*samuel.setName("sam");
+	samuel.setHitPoint(150);
+	samuel.setAttackDamage(40);
+	samuel.setEnergyPoint(100);*/
 
 	return 0;
 }
